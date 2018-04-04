f2c3671e99ce46cca989dcd5c517de37ffdb8f70
Jonathan Sherrah <jonathan.sherrah@gmail.com>
2018-04-04 11:09:41 +1000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jonathan Sherrah on 29/3/18.
+//  Copyright © 2018 Jonathan Sherrah. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
